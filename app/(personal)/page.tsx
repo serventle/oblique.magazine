f50edc4f4e7a5a61d309816b1eed2e14f0af28d1
serventle <gr316122017@gmail.com>
2026-04-23 16:12:37 +0500

    Update page.tsx

--- app/(personal)/page.tsx
+++ app/(personal)/page.tsx
@@ -3,9 +3,6 @@ import {sanityFetch} from '@/sanity/lib/live'
 import {homePageQuery} from '@/sanity/lib/queries'
 
 export default async function IndexRoute() {
-  // Мы запрашиваем данные, но теперь не блокируем страницу, если их нет
   const {data} = await sanityFetch({query: homePageQuery})
-
-  // Просто возвращаем наш дизайн. Он сам покажет демо-данные, если Sanity пуст.
   return <HomePage data={data} />
 }
