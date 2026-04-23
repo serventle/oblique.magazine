77c26cb3b16f937ba693c7fae180acae3578d146
serventle <gr316122017@gmail.com>
2026-04-23 14:07:04 +0500

    Update page.tsx

--- app/(personal)/page.tsx
+++ app/(personal)/page.tsx
@@ -1,23 +1,11 @@
 import {HomePage} from '@/components/HomePage'
-import {studioUrl} from '@/sanity/lib/api'
 import {sanityFetch} from '@/sanity/lib/live'
 import {homePageQuery} from '@/sanity/lib/queries'
-import Link from 'next/link'
 
 export default async function IndexRoute() {
+  // Мы запрашиваем данные, но теперь не блокируем страницу, если их нет
   const {data} = await sanityFetch({query: homePageQuery})
 
-  if (!data) {
-    return (
-      <div className="text-center">
-        You don&rsquo;t have a homepage yet,{' '}
-        <Link href={`${studioUrl}/structure/home`} className="underline">
-          create one now
-        </Link>
-        !
-      </div>
-    )
-  }
-
+  // Просто возвращаем наш дизайн. Он сам покажет демо-данные, если Sanity пуст.
   return <HomePage data={data} />
 }
